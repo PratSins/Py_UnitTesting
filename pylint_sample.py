df89f6e2522f4267487d975973e75c422fc00981
PratSins <pratyush2002ps@gmail.com>
2023-04-06 16:42:57 +0530

    Added the Project

---
+++ pylint_sample.py
@@ -0,0 +1,16 @@
+
+''' 
+        >>> python -m pylint pylint_sample.py
+          --- to use pylint for unit test  
+          
+        >>> python -m pylint pylint_sample.py -r y
+          --- to get a detailed report
+'''
+
+def myfunc():
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
